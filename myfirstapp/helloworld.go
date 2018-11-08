71db1d9700a0b1f838e30a098e0c2a1cf1d4f777
Geunyoung Kim <kgysmart@gmail.com>
2018-11-08 11:09:36 -0800

    Add HelloWorld

---
+++ myfirstapp/helloworld.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/gy-kim/mastering-go-programming/myfirstapp/utility"
+)
+
+func main() {
+	fmt.Println("Hello Go World!")
+	utility.SayHello()
+	utility.SayGY()
+}
---
+++ myfirstapp/utility/utility.go
@@ -0,0 +1,11 @@
+package utility
+
+import "fmt"
+
+func SayHello() {
+	fmt.Println("Hello")
+}
+
+func SayGY() {
+	fmt.Println("Hello GY")
+}
