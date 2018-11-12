88685d18abed38f4c7fd62f02674dfcc55f2ee80
Geunyoung Kim <kgysmart@gmail.com>
2018-11-11 23:11:50 -0800

    Add removing slice

--- section3/3-1/3-1.go
+++ section3/3-1/3-1.go
@@ -13,6 +13,14 @@ func main() {
 	subSlices()
 	fmt.Println("//////////// Append ////////////")
 	appendSlice()
+	fmt.Println("//////////// Remove Slice ////////")
+	removeSlice()
+}
+
+func removeSlice() {
+	a := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	a = append(a[:2], a[4:]...)
+	fmt.Println(a)
 }
 
 func appendSlice() {
