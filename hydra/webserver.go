612cc3a19853a1de0b3c2278bfdc81108680fbd1
Geunyoung Kim <kgysmart@gmail.com>
2018-11-15 11:29:29 -0800

    Add logger

--- hydra/webserver.go
+++ hydra/webserver.go
@@ -3,13 +3,20 @@ package main
 import (
 	"fmt"
 	"net/http"
+
+	"github.com/gy-kim/mastering-go-programming/hydra/hlogger"
 )
 
 func main() {
+	logger := hlogger.GetInstance()
+	logger.Println("Starting Hydra web service")
 	http.HandleFunc("/", sroot)
 	http.ListenAndServe(":8000", nil)
 }
 
 func sroot(w http.ResponseWriter, r *http.Request) {
+	logger := hlogger.GetInstance()
 	fmt.Fprintf(w, "Welcome to the Hydra software system")
+
+	logger.Println("Received an http Get request on root url")
 }
