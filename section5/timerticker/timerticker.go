7dde30bd8b7b4131bc131c6213befdad44c2bd0f
Geunyoung Kim <kgysmart@gmail.com>
2018-11-16 12:32:50 -0800

    Add cancel timer

--- section5/timerticker/timerticker.go
+++ section5/timerticker/timerticker.go
@@ -6,9 +6,46 @@ import (
 )
 
 func main() {
-	fmt.Println("//////// Timer ///////")
-	timer()
+	// fmt.Println("//////// Timer ///////")
+	// timer()
+	fmt.Println("//////// Cancel timer outside go routine ///////////")
+	cancelTimer()
+}
 
+func cancelTimer() {
+	nc := make(chan int)
+	stopc := make(chan bool)
+
+	go CancelSlowCounter(1, nc, stopc)
+	time.Sleep(5 * time.Second)
+
+	nc <- 2
+	time.Sleep(6 * time.Second)
+	stopc <- true
+	time.Sleep(1 * time.Second)
+}
+
+func CancelSlowCounter(n int, nc chan int, stopc chan bool) {
+	i := 0
+	// Create a duration of n seconds
+	d := time.Duration(n) * time.Second
+
+Loop:
+	for {
+		select {
+		// Use time.After channel to wait for a time period
+		case <-time.After(d):
+			i++
+			fmt.Println(i)
+		case n = <-nc:
+			fmt.Println("Timer duration changed to", n)
+		case <-stopc:
+			fmt.Println("Timer stopped")
+			break Loop
+		}
+	}
+
+	fmt.Println("Exiting slow Counter")
 }
 
 func timer() {
