afc1db91600d261fd33dcf76b4ed72abe256a4ac
Geunyoung Kim <kgysmart@gmail.com>
2018-11-16 12:49:51 -0800

    Add cancel timer

--- section5/timerticker/timerticker.go
+++ section5/timerticker/timerticker.go
@@ -8,8 +8,43 @@ import (
 func main() {
 	// fmt.Println("//////// Timer ///////")
 	// timer()
-	fmt.Println("//////// Cancel timer outside go routine ///////////")
-	cancelTimer()
+
+	// fmt.Println("//////// Cancel timer outside go routine ///////////")
+	// cancelTimer()
+
+	fmt.Println("///////// ticker ///////////")
+	ticker()
+}
+
+func ticker() {
+	tickC := make(chan *time.Ticker)
+	done := make(chan bool)
+	go tickerCounter(1, tickC, done)
+	ticker := <-tickC
+	time.Sleep(5 * time.Second)
+	ticker.Stop()
+	done <- true
+	time.Sleep(15 * time.Second)
+	fmt.Println("Exiting main..")
+}
+
+func tickerCounter(n int, tickC chan *time.Ticker, done <-chan bool) {
+	ticker := time.NewTicker(time.Duration(n) * time.Second)
+	tickC <- ticker
+	i := 0
+Loop:
+	for {
+		select {
+		case t := <-ticker.C:
+			i++
+			fmt.Println("Count", i, "at", t)
+		case <-done:
+			fmt.Println("done signal")
+			break Loop
+		}
+	}
+
+	fmt.Println("Exiting tick counter ...")
 }
 
 func cancelTimer() {
