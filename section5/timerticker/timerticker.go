b51ebf8728ad90196507a52662cfe40133dcd8ee
Geunyoung Kim <kgysmart@gmail.com>
2018-11-16 12:20:14 -0800

    Add timer

---
+++ section5/timerticker/timerticker.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	fmt.Println("//////// Timer ///////")
+	timer()
+}
+
+func timer() {
+	go SlowCounter(2)
+	time.Sleep(15 * time.Second)
+}
+
+func SlowCounter(n int) {
+	i := 0
+	// create a duration of n seconds
+	d := time.Duration(n) * time.Second
+
+	for {
+		// Create a timer for this duration
+		t := time.NewTimer(d)
+		<-t.C
+		i++
+		fmt.Println(i)
+	}
+}
