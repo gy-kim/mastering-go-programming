6eed5099a8c9c54f509d08623494e898c9cd955d
Geunyoung Kim <kgysmart@gmail.com>
2018-11-18 22:33:42 -0800

    Add hchatserver test

---
+++ hydra/hydrachat/hchatservertest/hchatservertest.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/gy-kim/mastering-go-programming/hydra/hydrachat"
+
+func main() {
+	hydrachat.Run(":2100")
+}
--- hydra/hydrachat/hydrachat.go
+++ hydra/hydrachat/hydrachat.go
@@ -12,23 +12,23 @@ import (
 
 var logger = hlogger.GetInstance()
 
-func Run() error {
-	l, err := net.Listen("tcp", ":2100")
+func Run(connection string) error {
+	l, err := net.Listen("tcp", connection)
 	r := CreateRoom("HydraChat")
 	if err != nil {
 		logger.Println("Error connecting to chat client", err)
 		return err
 	}
-	go func(l net.Listener) {
-		for {
-			conn, err := l.Accept()
-			if err != nil {
-				logger.Println("Error accepting connection from chat client", err)
-				break
-			}
-			go handleConnection(r, conn)
-		}
-	}(l)
+	// go func(l net.Listener) {
+	// 	for {
+	// 		conn, err := l.Accept()
+	// 		if err != nil {
+	// 			logger.Println("Error accepting connection from chat client", err)
+	// 			break
+	// 		}
+	// 		go handleConnection(r, conn)
+	// 	}
+	// }(l)
 
 	go func() {
 		// Handle SIGINT and SIGTERM.
@@ -45,7 +45,16 @@ func Run() error {
 		os.Exit(0)
 	}()
 
-	return nil
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			logger.Println("Error accepting connection from chat client", err)
+			break
+		}
+		go handleConnection(r, conn)
+	}
+
+	return err
 }
 
 func handleConnection(r *room, c net.Conn) {
