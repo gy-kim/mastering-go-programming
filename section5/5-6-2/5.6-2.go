6037f9163c324b776490e1ee8c7ab9186ad70c61
Geunyoung Kim <kgysmart@gmail.com>
2018-11-19 09:27:50 -0800

    Add reflect

---
+++ section5/5-6-2/5.6-2.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"fmt"
+	"reflect"
+)
+
+func main() {
+	// Explore a new type
+	var x1 float32 = 5.8
+	v := reflect.ValueOf(x1)
+	fmt.Println("type:", v.Type())
+	fmt.Println("Kind is float64?", v.Kind() == reflect.Float64)
+	fmt.Println("value:", v.Float())
+
+	// inspectTypeFloat(x1)
+
+	// we can use Interface() to get the interface value back from the reflect object
+	interfaceValue := v.Interface()
+	switch t := interfaceValue.(type) {
+	case float32:
+		fmt.Println("Float32 value", t)
+	case float64:
+		fmt.Println("Float64 value", t)
+	}
+
+	fmt.Println("v settable?", v.CanSet())
+
+	// get a reflect value object that represents a pointer to x1
+	vp := reflect.ValueOf(&x1)
+	fmt.Println("Type of vp", vp.Type(), "settable?", vp.CanSet())
+
+	// get a reflect value object representing the value that the pointer refers to
+	vpElement := vp.Elem()
+	fmt.Println("Type of vpElement", vpElement.Type(), "settable?", vpElement.CanSet())
+	vpElement.SetFloat(2.2)
+	fmt.Println(x1)
+
+	// For simplicity, the getter provides the largest type of a value. So instead of float32 it returns float32 it returns float64
+	x2 := v.Float()
+	fmt.Println(reflect.TypeOf(x2))
+
+	// Reflection gets the underlying type instead of the custom/static type
+	type myfloat float64
+	var x3 myfloat = 5.87
+	v = reflect.ValueOf(x3)
+	fmt.Println(v.Type(), "kind is float64?", v.Kind() == reflect.Float64)
+}
+
+func inspectTypeFloat(i interface{}) {
+	v := reflect.ValueOf(i)
+	fmt.Println("type:", v.Type())
+	fmt.Println("Kind is float64?", v.Kind() == reflect.Float64)
+	fmt.Println("Value:", v.Float())
+}
